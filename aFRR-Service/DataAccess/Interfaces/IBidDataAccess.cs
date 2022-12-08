fd23aa6edf3bbb164594ec6f7a7108fd55859ecf
WrexBG <40688355+WrexBG@users.noreply.github.com>
2022-12-08 14:45:04 +0100

    Code Cleanup for IBidDataAccess and ISignalDataAccess
    removed unused usings
    Added usings for Interfaces and models
    switched to class global namespace

--- aFRR-Service/DataAccess/Interfaces/IBidDataAccess.cs
+++ aFRR-Service/DataAccess/Interfaces/IBidDataAccess.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DataAccess.Interfaces;
+using DataAccess.Models;
 
-namespace DataAccessLayer.Interfaces
+namespace DataAccessLayer.Interfaces;
+
+internal interface IBidDataAccess : IBaseDataAccess<Bid>
 {
-    internal interface IBidDataAccess : IBaseDataAccess<Bid>
-    {
-    }
 }
--- aFRR-Service/DataAccess/Interfaces/ISignalDataAccess.cs
+++ aFRR-Service/DataAccess/Interfaces/ISignalDataAccess.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DataAccess.Interfaces;
+using DataAccess.Models;
 
-namespace DataAccessLayer.Interfaces
+namespace DataAccessLayer.Interfaces;
+
+internal interface ISignalDataAccess : IBaseDataAccess<Signal>
 {
-    internal interface ISignalDataAccess : IBaseDataAccess<Signal>
-    {
-    }
 }
