e535df942a6afc256fd65f387f376e6e98813a70
Martin Vlacil <martinvlacil.mv@gmail.com>
2022-12-08 14:58:32 +0100

    Adding Data Access Factory

--- aFRR-Service/DataAccess/DataAccessFactory.cs
+++ aFRR-Service/DataAccess/DataAccessFactory.cs
@@ -4,9 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessLayer
+namespace DataAccessLayer;
+public class DataAccessFactory
 {
-    internal class DataAccessFactory
+    public static T GetDataAccess<T>(string connectionString) where T : class
     {
+        switch (typeof(T).Name)
+        {
+            case "IBookingDataAccess": return new SignalDataAccess(connectionString) as T;
+            case "ICinemaDataAccess": return new BidDataAccess(connectionString) as T;
+            default:
+                break;
+        }
+        throw new ArgumentException($"Unknown type {typeof(T).FullName}");
     }
 }
+
