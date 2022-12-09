65b0cc9ca9da6c3ea81b3c301e3c985edeb457d4
WrexBG <40688355+WrexBG@users.noreply.github.com>
2022-12-09 11:47:36 +0100

    Updated SignalDataAccess
    commented out unfinished code
    added a todo for changing the insert command

--- aFRR-Service/DataAccess/DataAccess/SignalDataAccess.cs
+++ aFRR-Service/DataAccess/DataAccess/SignalDataAccess.cs
@@ -1,16 +1,15 @@
-﻿using Dapper;
-using DataAccessLayer.Interfaces;
+﻿using DataAccessLayer.Interfaces;
 using DataAccessLayer.Models;
-using System.Data.SqlClient;
 
 namespace DataAccessLayer.DataAccess;
 internal class SignalDataAccess : BaseDataAccess<Signal>, ISignalDataAccess
 {
     internal SignalDataAccess(string connectionString) : base(connectionString)
     {
+        //TODO: Set the InsertCommand protected property of BaseDataAccess to an SQL Query that Inserts a new Signal and Inserts an entry in the signal bid member table
     }
 
-    internal async override Task<int> CreateAsync(Signal signal) 
+    /*internal async override Task<int> CreateAsync(Signal signal) 
     {
         int signalId;
         string signalCommand = $"INSERT INTO Signal" +
@@ -21,5 +20,5 @@ internal class SignalDataAccess : BaseDataAccess<Signal>, ISignalDataAccess
             using var connection = CreateConnection();
             signalId = await connection.QuerySingleAsync<int>(signalCommand, new {Id = signal.ID, FromUtc = signal.FromUtc, ToUtc = signal.ToUtc, Price = signal.Price, CurrencyId = signal.CurrencyId, QuantityMw = signal.QuantityMw, DirectionId = signal.DirectionId});
         }
-    }
+    }*/
 }
