78815a02d2d7f39d431c39f53ed3a283bdee21de
Martin Vlacil <martinvlacil.mv@gmail.com>
2022-12-09 11:08:55 +0100

    Added a Configuration file and SignalDataAccess base logic

--- aFRR-Service/DataAccess/DataAccess/SignalDataAccess.cs
+++ aFRR-Service/DataAccess/DataAccess/SignalDataAccess.cs
@@ -1,10 +1,25 @@
-﻿using DataAccessLayer.Interfaces;
+﻿using Dapper;
+using DataAccessLayer.Interfaces;
 using DataAccessLayer.Models;
+using System.Data.SqlClient;
 
 namespace DataAccessLayer.DataAccess;
 internal class SignalDataAccess : BaseDataAccess<Signal>, ISignalDataAccess
 {
-    public SignalDataAccess(string connectionString) : base(connectionString)
+    internal SignalDataAccess(string connectionString) : base(connectionString)
     {
+    }
+
+    internal async override Task<int> CreateAsync(Signal signal) 
+    {
+        int signalId;
+        string signalCommand = $"INSERT INTO Signal" +
+            $"OUTPUT INSERTED.[Id]" +
+            $"VALUES(@Id, @FromUtc, @ToUtc, @Price, @CurrencyId, @QuantityMw, @DirectionId)";
+        try
+        {
+            using var connection = CreateConnection();
+            signalId = await connection.QuerySingleAsync<int>(signalCommand, new {Id = signal.ID, FromUtc = signal.FromUtc, ToUtc = signal.ToUtc, Price = signal.Price, CurrencyId = signal.CurrencyId, QuantityMw = signal.QuantityMw, DirectionId = signal.DirectionId});
+        }
     }
 }
