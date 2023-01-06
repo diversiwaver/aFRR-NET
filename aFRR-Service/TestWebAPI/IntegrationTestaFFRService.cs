dd45ab66284265b2f08a795a24ce8c02ddc8d719
WrexBG <40688355+WrexBG@users.noreply.github.com>
2023-01-06 17:49:46 +0100

    Integration test for Web API
    Integration test between WebAPI and DataAccess with the test database

---
+++ aFRR-Service/TestWebAPI/IntegrationTestaFFRService.cs
@@ -0,0 +1,68 @@
+﻿using aFRRService.DTOs;
+using BaseDataAccess.Interfaces;
+using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using TestWebAPI.Stubs;
+using WebAPI.Controllers;
+
+namespace TestWebAPI;
+internal class IntegrationTestaFFRService
+{
+    SignalsController _webApiController;
+    Random _random;
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        ISignalDataAccess signalDataAccess = DataAccessFactory.GetDataAccess<ISignalDataAccess>(Configuration.CONNECTION_STRING_TEST);
+        _webApiController = new SignalsController(signalDataAccess, new Mock<ILogger<SignalsController>>().Object);
+        _random = new Random();
+    }
+
+    [Test]
+    public async Task SignalsController_ShouldReturnCreatedSignalId()
+    {
+        //Arrange
+        decimal price = (decimal)(_random.NextDouble() * 15 + 10);
+        decimal quantity = (decimal)(_random.NextDouble() * 15 + 10);
+        SignalDTO newSignalDto = new() { Id = 0, CurrencyId = 0, FromUtc = DateTime.UtcNow, ToUtc = DateTime.UtcNow.AddHours(1), Price = price, QuantityMw = quantity, DirectionId = 0, BidId = 0 };
+        //Act
+        var idActionResult = (await _webApiController.PostAsync(newSignalDto)).Result;
+        if (idActionResult is ObjectResult objRes)
+        {
+            // Assert
+            Assert.That(objRes.StatusCode, Is.EqualTo(200), "Status code returned was not 200");
+
+            int returnedId = (int)objRes.Value;
+            Assert.That(returnedId, Is.Not.EqualTo(-1), "Returned Id was -1, indicating a fail in the data access");
+            newSignalDto.Id = returnedId;
+            Assert.That(returnedId, Is.EqualTo(newSignalDto.Id), "Signal wasn't created");
+        }
+        else if (idActionResult is StatusCodeResult scr)
+        {
+            // Assert
+            Assert.That(scr.StatusCode, Is.EqualTo(200));
+        }
+    }
+
+    [Test]
+    public async Task SignalsController_ShouldGetAllSignals()
+    {
+        //Arrange
+        IEnumerable<SignalDTO>? signalDtos;
+
+        //Act
+        var actionResult = (await _webApiController.GetAllAsync()).Result;
+        ObjectResult? objectResult = actionResult as ObjectResult;
+        signalDtos = objectResult.Value as IEnumerable<SignalDTO>;
+        Assert.Multiple(() =>
+        {
+            //Assert
+            Assert.That(signalDtos, Is.Not.Null, "signalDtos list was null");
+            Assert.That(objectResult.StatusCode, Is.EqualTo(200), "Status code was not OK(200)");
+            Assert.That(signalDtos.Any(), Is.True, "List is currently empty");
+        });
+    }
+}
