848d9c8efdcb377c171e5205a820b700500d0c98
Martin Vlacil <martinvlacil.mv@gmail.com>
2023-01-06 15:12:09 +0100

    Remote control API consumer

---
+++ aFRR-Service/DataAccess/Interfaces/IRemoteControlDataAccess.cs
@@ -0,0 +1,8 @@
+﻿using aFRRService.DTOs;
+
+namespace DataAccessLayer.Interfaces;
+
+public interface IRemoteControlDataAccess
+{
+    Task<bool> SendAsync(SignalDTO signalDto);
+}
---
+++ aFRR-Service/TestDataAccess/Tests/TestRemoteControl.cs
@@ -0,0 +1,88 @@
+﻿using aFRRService.DTOs;
+using DataAccessLayer;
+using DataAccessLayer.Interfaces;
+using Moq;
+using Moq.Protected;
+using System.Net;
+using System.Reflection;
+
+namespace TestDataAccess.Tests;
+public class TestRemoteControl
+{
+    private IRemoteControlDataAccess _dataAccess;
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+        handlerMock
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>()
+            )
+            .ReturnsAsync(new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+            })
+            .Verifiable();
+
+        HttpClient mockHTTPClient = new HttpClient(handlerMock.Object)
+        {
+            BaseAddress = new Uri("http://test.com/"),
+        };
+
+        _dataAccess = DataAccessFactory.GetDataAccess<IRemoteControlDataAccess>(mockHTTPClient);
+    }
+
+    [Test]
+    public async Task RemoteControlDataAccess_ShouldReturnTrue_WhenSendingSignal()
+    {
+        //Arrange
+        SignalDTO signalDTO = new SignalDTO()
+        {
+            Id = 1,
+            FromUtc = DateTime.UtcNow,
+            ToUtc = DateTime.UtcNow.AddHours(1),
+            Price = 20,
+            CurrencyId = 1,
+            QuantityMw = 10,
+            DirectionId = 0,
+            BidId = 0,
+            AssetsToRegulate = new List<AssetDTO>()
+            {
+                new AssetDTO
+                {
+                    Id = 33,
+                    AssetGroupId = 2,
+                    Location = null,
+                    CapacityMw = 10,
+                    RegulationPercentage = 100
+                },
+                new AssetDTO
+                {
+                    Id = 21,
+                    AssetGroupId = 0,
+                    Location = null,
+                    CapacityMw = 8,
+                    RegulationPercentage = 100
+                },
+                new AssetDTO
+                {
+                    Id = 31,
+                    AssetGroupId = 2,
+                    Location = null,
+                    CapacityMw = 8,
+                    RegulationPercentage = 25
+                }
+            }
+        };
+
+        //Act
+        var returnedSignal = (await _dataAccess.SendAsync(signalDTO));
+
+        //Assert
+        Assert.That(returnedSignal, Is.True, "Sending signal returned False.");
+    }   
+}
