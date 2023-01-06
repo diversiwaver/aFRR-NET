952f408fb0a8246c950f3f25fc79c2850254466e
Mette Thorhauge <mettekt94@gmail.com>
2023-01-06 12:38:02 +0100

    Adding Json property names to DTOs
    The DTOs for Assets and Signals now have  specified JsonPropertyNames to allow mapping from json upon deserialization

--- aFRR-Service/DtoConverters/SignalDTO.cs
+++ aFRR-Service/DtoConverters/SignalDTO.cs
@@ -1,14 +1,33 @@
-﻿namespace aFRRService.DTOs;
+﻿using System.Text.Json.Serialization;
+
+namespace aFRRService.DTOs;
 
 public class SignalDTO
 {
+    [JsonPropertyName ("id")]
     public int Id { get; set; }
+
+    [JsonPropertyName("fromUtc")]
     public DateTime FromUtc { get; set; }
+
+    [JsonPropertyName("toUtc")]
     public DateTime ToUtc { get; set; }
+
+    [JsonPropertyName("price")]
     public decimal Price { get; set; }
+
+    [JsonPropertyName("currencyId")]
     public int CurrencyId { get; set; }
+
+    [JsonPropertyName("quantityMw")]
     public decimal QuantityMw { get; set; }
+
+    [JsonPropertyName("directionId")]
     public int DirectionId { get; set; }
+
+    [JsonPropertyName("bidId")]
     public int BidId { get; set; }
+
+    [JsonPropertyName("assetsToRegulate")]
     public IEnumerable<AssetDTO>? AssetsToRegulate { get; set; }
 }
